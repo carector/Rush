4558e7250b92aab530a4701a3b30613404649056
Basel <baseline7@live.com>
2020-05-25 14:38:54 -0700

    Initial Commit

---
+++ RushRecreation/Assets/GreenBounce.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenBounce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Bounce());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Bounce()
+    {
+        float bouncePosY = -0.1f + Random.Range(-0.025f, 0.01f);
+
+        while(Mathf.Abs(bouncePosY - transform.localPosition.y) >= 0.01f)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, bouncePosY, transform.localPosition.z), 0.7f);
+            yield return null;
+        }
+
+        StartCoroutine(Bounce());
+    }
+}
---
+++ RushRecreation/Assets/PinkBounce.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinkBounce : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Bounce()
+    {
+        float bouncePosY = Random.Range(-0.00005f, 0.00005f);
+
+        while (Mathf.Abs(bouncePosY - transform.localPosition.y) >= 0.00001f)
+        {
+            transform.localPosition = Vector3.Lerp(transform.localPosition, new Vector3(transform.localPosition.x, bouncePosY, transform.localPosition.z), 0.3f);
+            yield return null;
+        }
+
+        StartCoroutine(Bounce());
+    }
+}
